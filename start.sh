89b71e3d03f0461efa6184deefdbe88c0d508332
Filippo Gibertini <filippogib@gmail.com>
2025-10-18 10:11:43 +0000

    minor tweak in start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ xhost +local:docker
 
 docker compose up -d --build
 
-docker compose exec pcl_app /bin/bash
+#docker compose exec pcl_app /bin/bash
 
 echo "-------------------------------------------------------------------"
 echo "INFO: Exited container shell. The container remains running in the background."
